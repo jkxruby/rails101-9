fff67d7464c1dd861553f8917c187be6b6b17fb0
jkxruby <jkxruby@gmail.com>
2017-07-28 17:27:37 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
 
+
 def index
   @groups = Group.all
 end
@@ -39,6 +40,6 @@ end
 private
 def group_params
   params.require(:group).permit(:title, :description)
-end 
+end
 
 end
